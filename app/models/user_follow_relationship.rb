035c50a881c2d9c0bc8828037137ac1aa5c68cf8
KienBui <doankien.bui@gmail.com>
2013-04-02 19:16:14 +0700

    fix

--- app/models/user_follow_relationship.rb
+++ app/models/user_follow_relationship.rb
@@ -1,6 +1,8 @@
 class UserFollowRelationship < ActiveRecord::Base
 	attr_accessible :following_id,:follower_id
 
-	belongs_to :user,  class_name: "User"
+  belongs_to :follower, class_name: "User"
+	belongs_to :following, class_name: "User"
+
 
 end
