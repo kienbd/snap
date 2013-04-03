22f0e1656d03663c0b632901c73e9cb37532ba35
DxTa <ductm310@gmail.com>
2013-04-03 23:30:20 +0700

    signup facebook auto verified

--- db/schema.rb
+++ db/schema.rb
@@ -75,10 +75,10 @@ ActiveRecord::Schema.define(:version => 20130402063122) do
   create_table "photos", :force => true do |t|
     t.string   "description"
     t.string   "name"
-    t.string   "source"
     t.integer  "box_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "origin_owner_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.string   "image"
   end
 
