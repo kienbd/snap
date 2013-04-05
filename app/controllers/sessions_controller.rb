e060e9f25e06a5ccb3251f27c97ec60c3895c08e
DxTa <ductm310@gmail.com>
2013-04-05 10:18:04 +0700

    welcome page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       unless @user.verify?
-        redirect_to root_path, notice: "Please verify your email!" 
+        redirect_to root_path, notice: "Please verify your email!"
       else
         sign_in @user
         redirect_back_or root_path
